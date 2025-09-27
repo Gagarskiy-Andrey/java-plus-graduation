7e60dbb53c9c96928855abd4aa7b0a309e9f9f13
GrimJak <a.gagarskiy@yandex.kz>
2025-09-27 16:37:34 +0300

    Этап 2

--- core/interaction-api/src/main/java/ru/practicum/explorewithme/api/utility/UserMapper.java
+++ core/interaction-api/src/main/java/ru/practicum/explorewithme/api/utility/UserMapper.java
@@ -2,10 +2,12 @@ package ru.practicum.explorewithme.api.utility;
 
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.practicum.explorewithme.api.client.user.dto.UserDto;
 import ru.practicum.explorewithme.api.client.user.dto.UserShortDto;
 
 @Mapper(componentModel = "spring")
+@Component("userMapperApiImpl")
 public interface UserMapper {
 
     UserShortDto toUserShortDto(UserDto userDto);
